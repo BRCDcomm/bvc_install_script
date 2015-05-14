65b4c62f1fda45b9859051ec1514f1ed89bdc50f
Jim Burns <jeb@elbrys.com>
2015-05-14 17:22:44 -0400

    Edit script and README.md for version 1.3 of BVC

--- install_bvc.sh
+++ install_bvc.sh
@@ -30,8 +30,8 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # author: Alec Rooney
-# This script automates the install of BVC version 1.2.  With some changes it might
-# work with other versions of BVC, however, it has only been tested with 1.2.
+# This script automates the install of BVC version 1.3.  With some changes it might
+# work with other versions of BVC, however, it has only been tested with 1.3.
 # This script assumes you have installed and setup Ubuntu 14.04 as per Jim Burns 
 # video here:  https://www.youtube.com/watch?v=saNnY7A55gM
 # The BVC quick start instructions recommend installing BVC in the /opt directory.  
@@ -40,10 +40,10 @@
 # To use this script do the following:
 #
 # 1) copy/move this script to the /opt directory
-# 2) download bvc-1.2.0.zip and bvc-dependencies-1.2.0.zip from http://www.brocade.com/forms/jsp/vyatta-controller/index.jsp
-# 3) copy/move bvc-1.2.0.zip and bvc-dependencies-1.2.0.zip to the /opt directory
+# 2) download bvc-1.3.0.zip and bvc-dependencies-1.3.0.zip from http://www.brocade.com/forms/jsp/vyatta-controller/index.jsp
+# 3) copy/move bvc-1.3.0.zip and bvc-dependencies-1.3.0.zip to the /opt directory
 # 4) cd to the /opt directory and run the script as follows: sudo ./install_bvc.sh
-# 5) if the script runs successfully you should see BVC1.2 installed.  If not, you can always try running the steps manually as per the quick start guide from Brocade 
+# 5) if the script runs successfully you should see BVC1.3 installed.  If not, you can always try running the steps manually as per the quick start guide from Brocade 
 
 
 CURRENT_USER=`logname`
@@ -84,8 +84,8 @@ apt-get install -y openssh-server
 mkdir bvc
 chown $CURRENT_USER bvc
 
-BVC_ZIP=bvc-1.2.0.zip
-BVC_DEP_ZIP=bvc-dependencies-1.2.0.zip
+BVC_ZIP=bvc-1.3.0.zip
+BVC_DEP_ZIP=bvc-dependencies-1.3.0.zip
 
 if [ -f $BVC_ZIP ]; then
     sudo -u $CURRENT_USER unzip -o $BVC_ZIP 
@@ -112,7 +112,7 @@ if [ $? -eq 0 ]; then
     echo "Node.js:"
     echo `node --version`
     echo "openssh-server"
-    echo "BVC version 1.2"
+    echo "BVC version 1.3"
 else
     echo "Failed to install BVC and its dependencies!"
 fi
